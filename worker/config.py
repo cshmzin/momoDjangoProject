d08e18b2121f7b7fb738f2c6b602508fb49268f2
lenovo <1054880760@qq.com>
2022-09-23 15:50:24 +0800

    1.0.0 add timer

--- worker/config.py
+++ worker/config.py
@@ -1,3 +1,15 @@
 import time
 
-print(time.time())
+def timer(fcn):
+    def inner(*args, **kwargs):
+        start_time = time.time()
+        fcn(*args, **kwargs)
+        end_time = time.time()
+        print(end_time - start_time)
+    return inner
+
+@timer
+def Test():
+    time.sleep(1)
+
+test = Test()
